6e40cb90519e14ef214112be813d0bdcfab47d32
Daniel França <danieldrafts@gmail.com>
2021-02-27 20:50:32 -0300

    Configurando Swagger

--- curso.api/Startup.cs
+++ curso.api/Startup.cs
@@ -26,6 +26,7 @@ namespace curso.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,16 @@ namespace curso.api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
